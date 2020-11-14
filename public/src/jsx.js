03efd33927d3900d41f0f19ea04d7113fca3bfbd
hyeonsik <sun202x@gmail.com>
2020-11-14 20:28:23 +0900

    didact sample 코드 작성

---
+++ public/src/jsx.js
@@ -0,0 +1,80 @@
+/** @jsx Didact.createElement */
+const Didact = importFromBelow();
+
+const stories = [
+    { name: "Didact introduction", url: "http://bit.ly/2pX7HNn" },
+    { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH" },
+    { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S" },
+    { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746" },
+    { name: "Components and state", url: "http://bit.ly/2rE16nh" }
+];
+
+const appElement = <div><ul>{stories.map(storyElement)}</ul></div>;
+
+function storyElement({ name, url }) {
+    const likes = Math.ceil(Math.random() * 100);
+    return (
+        <li>
+            <button>{likes}❤️</button>
+            <a href={url}>{name}</a>
+        </li>
+    );
+}
+
+Didact.render(appElement, document.getElementById("root"));
+
+/** ⬇️⬇️⬇️⬇️⬇️ 🌼Didact🌼 ⬇️⬇️⬇️⬇️⬇️ **/
+
+function importFromBelow() {
+    const TEXT_ELEMENT = "TEXT ELEMENT";
+
+    function render(element, parentDom) {
+        // 우리가 정의해놓은 createElement의 결과 값이 들어온다.
+        const { type, props } = element;
+
+        // Create DOM element
+        const isTextElement = type === TEXT_ELEMENT;
+        const dom = isTextElement
+            ? document.createTextNode("")
+            : document.createElement(type);
+
+        // Add event listeners
+        const isListener = name => name.startsWith("on");
+        Object.keys(props).filter(isListener).forEach(name => {
+            const eventType = name.toLowerCase().substring(2);
+            dom.addEventListener(eventType, props[name]);
+        });
+
+        // Set properties
+        const isAttribute = name => !isListener(name) && name != "children";
+        Object.keys(props).filter(isAttribute).forEach(name => {
+            dom[name] = props[name];
+        });
+
+        // Render children
+        const childElements = props.children || [];
+        childElements.forEach(childElement => render(childElement, dom));
+
+        // Append to parent
+        parentDom.appendChild(dom);
+    }
+
+    function createElement(type, config, ...args) {
+        const props = Object.assign({}, config);
+        const hasChildren = args.length > 0;
+        const rawChildren = hasChildren ? [].concat(...args) : [];
+        props.children = rawChildren
+            .filter(c => c != null && c !== false)
+            .map(c => c instanceof Object ? c : createTextElement(c));
+        return { type, props };
+    }
+
+    function createTextElement(value) {
+        return createElement(TEXT_ELEMENT, { nodeValue: value });
+    }
+
+    return {
+        render,
+        createElement
+    };
+}
